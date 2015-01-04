ba0b22bf0ce4cc6fd62fea585bceacee16661225
nlance3 <nickdlance@gmail.com>
2015-01-04 16:48:36 -0700

    full thing?

---
+++ android/src/com/BwB/game/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.BwB.game.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.BwB.game.BwB;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new BwB(), config);
+	}
+}
---
+++ html/src/com/BwB/game/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.BwB.game.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.BwB.game.BwB;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new BwB();
+        }
+}
